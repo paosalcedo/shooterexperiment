e3ae90fc883b65048c3d3ac6130845a4765072a3
Unknown <paosalcedo@gmail.com>
2017-08-23 01:24:58 -0400

    Added Probuilder, New "ThrowYourself" Scene

---
+++ ShooterExperiment/ShooterExperiment/Assets/PlayerSwitcherScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSwitcherScript : MonoBehaviour {
+
+	public GameObject Player1;
+	public GameObject Player2;
+	public GameObject MainCamera;
+
+	public enum CurrentPlayer{
+		PLAYER1,
+		PLAYER2
+	}
+
+	CurrentPlayer currentPlayer;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SwitchPlayer(){
+		if (currentPlayer == CurrentPlayer.PLAYER1) {
+			MainCamera.transform.SetParent (Player2.transform);	
+		}
+
+		if (currentPlayer == CurrentPlayer.PLAYER2) {
+			MainCamera.transform.SetParent (Player1.transform);
+		}
+	}
+}
---
+++ ShooterExperiment/ShooterExperiment/Assets/Scripts/PlayerSwitcherScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSwitcherScript : MonoBehaviour {
+
+	public GameObject player1;
+	public GameObject player2;
+	public GameObject mainCamera;
+	public KeyCode switchKey;
+
+	public static GameObject currentParent;
+
+	public enum CurrentPlayer{
+		PLAYER1,
+		PLAYER2
+	}
+
+	CurrentPlayer currentPlayer;
+	// Use this for initialization
+	void Start () {
+		currentPlayer = CurrentPlayer.PLAYER1;
+		currentParent = player1;
+		switchKey = KeyCode.Q;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		SwitchPlayer (switchKey);	
+	}
+
+	public void SwitchPlayer(KeyCode key){
+		if (Input.GetKeyDown(key)) {
+			if (currentPlayer == CurrentPlayer.PLAYER1) {
+				mainCamera.transform.SetParent (player2.transform);
+				mainCamera.transform.position = player2.transform.position;
+				player1.GetComponent<FPSController> ().enabled = false;
+				player2.GetComponent<FPSController> ().enabled = true;
+				currentPlayer = CurrentPlayer.PLAYER2;
+				currentParent = player2;
+				return;
+			}
+
+			if (currentPlayer == CurrentPlayer.PLAYER2) {
+				mainCamera.transform.SetParent (player1.transform);
+				mainCamera.transform.position = player1.transform.position;
+				player2.GetComponent<FPSController> ().enabled = false;
+				player1.GetComponent<FPSController> ().enabled = true;
+				currentPlayer = CurrentPlayer.PLAYER1;
+				currentParent = player1;
+ 				return;
+			}
+		}
+	}
+}
