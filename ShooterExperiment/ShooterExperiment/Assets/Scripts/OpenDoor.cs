3bf4800a9ad7c14f0748f94b917d87b4407981e2
Unknown <paosalcedo@gmail.com>
2017-08-23 17:10:10 -0400

    More level components added
    Trigger, Door, scripts for opening

---
+++ ShooterExperiment/ShooterExperiment/Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour {
+
+	public enum DoorState{
+		OPEN,
+		CLOSED
+	}
+
+	DoorState doorState;
+	// Use this for initialization
+	void Start () {
+		doorState = DoorState.CLOSED;
+ 	}
+	
+	// Update is called once per frame
+	void Update ()
+	{		
+	}
+
+	public void Open(){
+		this.gameObject.GetComponent<MeshRenderer>().enabled = false;
+		this.gameObject.GetComponent<Collider>().enabled = false;
+	}
+
+	public void Close(){
+		this.gameObject.GetComponent<MeshRenderer>().enabled = true;
+		this.gameObject.GetComponent<Collider>().enabled = true;
+	}
+
+	
+
+	
+}
