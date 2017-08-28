a46fd0b838f6a4a77c0355151634e1c7a0330613
Unknown <paosalcedo@gmail.com>
2017-08-28 16:37:21 -0400

    Added simple trigger functionality to P1 obstacle 2

--- ShooterExperiment/ShooterExperiment/Assets/Scripts/ObstacleTween.cs
+++ ShooterExperiment/ShooterExperiment/Assets/Scripts/ObstacleTween.cs
@@ -5,14 +5,25 @@ using UnityEngine;
 public class ObstacleTween : MonoBehaviour {
 	
 	public float delay;
+	public uint uid;
+	public uint uglobalid;
 	// Use this for initialization
 	void Start () {
-		LeanTween.moveLocalY(this.gameObject, 20, 1.5f).setEaseInSine().setLoopPingPong().setDelay(delay);		
+		LeanTween.moveLocalY(this.gameObject, 20, 0.5f).setEaseInSine().setLoopPingPong().setDelay(delay).setId(uid, 0);		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	public void PauseTween(){
+		LeanTween.pause (this.gameObject);
+//		LeanTween.pauseAll ();
+	}
+
+	public void ResumeTween(){
+		LeanTween.resume (this.gameObject);
 	}
 
 
---
+++ ShooterExperiment/ShooterExperiment/Assets/Scripts/TreadmillControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreadmillControl : MonoBehaviour {
+
+	public GameObject crusher1;
+	public GameObject crusher2;
+	public GameObject crusher3;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(){
+		crusher1.SendMessage ("PauseTween");
+		crusher2.SendMessage ("PauseTween");
+		crusher3.SendMessage ("PauseTween");
+	}
+
+	void OnTriggerExit(){
+		crusher1.SendMessage ("ResumeTween");
+		crusher2.SendMessage ("ResumeTween");
+		crusher3.SendMessage ("ResumeTween");
+	}
+
+
+
+}
