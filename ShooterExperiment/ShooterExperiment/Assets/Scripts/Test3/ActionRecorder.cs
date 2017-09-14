2958cdfe2e78a8f87bd992daebd1133ee744c346
DESKTOP-4U4MIKI\Paolo <paosalcedo@gmail.com>
2017-09-14 03:17:19 -0400

    Fixed wrong first player in scene

--- ShooterExperiment/ShooterExperiment/Assets/Scripts/Test3/ActionRecorder.cs
+++ ShooterExperiment/ShooterExperiment/Assets/Scripts/Test3/ActionRecorder.cs
@@ -2,93 +2,135 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ActionRecorder : MonoBehaviour {
+public class ActionRecorder : MonoBehaviour
+{
 
-	bool pressed = false;
+    bool pressed = false;
 
-	public enum RecordingState{
-		RECORDING,
-		NOT_RECORDING,
-		PLAYBACK
-	}
+    public enum RecordingState
+    {
+        RECORDING,
+        NOT_RECORDING,
+        PLAYBACK
+    }
 
-	public KeyCode playKey;
-	public KeyCode recordKey;
+    public KeyCode playKey;
+    public KeyCode recordKey;
 
-	public RecordingState recordingState;
+    public RecordingState recordingState;
 
-	public List<Vector3> positions = new List<Vector3>();
-	// Use this for initialization
+    public List<Vector3> positions = new List<Vector3>();
+    // Use this for initialization
 
-	float t = 1f;
+    float t = 1f;
 
-	void Start () {
-		recordingState = RecordingState.NOT_RECORDING;
+    void Start()
+    {
+        recordingState = RecordingState.NOT_RECORDING;
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
 
-		ToggleRecord (recordKey);
-		PlayRecording (playKey);
+    // Update is called once per frame
+    void Update()
+    {
 
-		if (recordingState == RecordingState.RECORDING) {
-			RecordMovement (transform.position);
-		} 
+        ToggleRecord(recordKey);
 
-		if (recordingState == RecordingState.NOT_RECORDING) {
-			return;
- 		}
+        //      if (recordingState != RecordingState.RECORDING) { 
+        //          PlayRecording(playKey);
+        //      }
 
-		if (recordingState == RecordingState.PLAYBACK) {
-			MoveBasedOnRecording ();
-		}
-	}
+        //if (recordingState == RecordingState.RECORDING) {
+        //	RecordMovement (transform.position);
+        //} 
 
-	void ToggleRecord(KeyCode key){
-		if(Input.GetKeyDown(key)){
-			posAtRecordStart = transform.position;
+        //if (recordingState == RecordingState.NOT_RECORDING) {
+        //	return;
+        //	}
 
-			if (recordingState == RecordingState.NOT_RECORDING || recordingState == RecordingState.PLAYBACK) {
-				recordingState = RecordingState.RECORDING;
-				return;
-			}
-			if (recordingState == RecordingState.RECORDING || recordingState == RecordingState.PLAYBACK) {
-				recordingState = RecordingState.NOT_RECORDING;
- 				return;
-			}
-		}
-	}
+        //if (recordingState == RecordingState.PLAYBACK) {
+        //	MoveBasedOnRecording ();
+        //}
+    }
 
-	private Vector3 posAtRecordStart;
+    private void FixedUpdate()
+    {
+        Debug.Log("Last rec index: " + lastRecIndex);
 
-	void RecordMovement(Vector3 playerPos){
-		t -= Time.deltaTime;
-		if (t <= 0) {
-			positions.Add (playerPos);
-			t = 1f;
-		}
-	}
+        if (recordingState != RecordingState.RECORDING)
+        {
+            PlayRecording(playKey);
+        }
 
-	float w = 0.1f;
+        if (recordingState == RecordingState.RECORDING)
+        {
+            RecordMovement(transform.position);
+        }
 
-	private int nextIndex = 0;
+        if (recordingState == RecordingState.NOT_RECORDING)
+        {
+            return;
+        }
 
-	void MoveBasedOnRecording(){
-		w -= Time.deltaTime;
-		if (w <= 0) {
-			nextIndex++;
-			w = 1f;
-		}
-		transform.position = positions [nextIndex];
-	}
+        if (recordingState == RecordingState.PLAYBACK)
+        {
+            MoveBasedOnRecording();
+        }
 
-	void PlayRecording(KeyCode key){
-		if (Input.GetKeyDown (key)) {
-			recordingState = RecordingState.PLAYBACK;
-		}
-	}
+        if (playbackIndex >= positions.Count)
+        {
+            recordingState = RecordingState.NOT_RECORDING;
+        }
 
-	
+    }
+
+    private Vector3 posAtRecordStart;
+    private int lastRecIndex;
+
+    void ToggleRecord(KeyCode key)
+    {
+        if (Input.GetKeyDown(key))
+        {
+            posAtRecordStart = transform.position;
+
+            if (recordingState == RecordingState.NOT_RECORDING || recordingState == RecordingState.PLAYBACK)
+            {
+                recordingState = RecordingState.RECORDING;
+                return;
+            }
+            if (recordingState == RecordingState.RECORDING || recordingState == RecordingState.PLAYBACK)
+            {
+                recordingState = RecordingState.NOT_RECORDING;
+                lastRecIndex = positions.Count - 1;
+                return;
+            }
+        }
+    }
+
+
+    void RecordMovement(Vector3 playerPos)
+    {
+        positions.Add(playerPos);
+    }
+
+    float w = 0.1f;
+
+    private int playbackIndex = 0;
+
+    void MoveBasedOnRecording()
+    {
+        playbackIndex++;
+        Debug.Log(playbackIndex);
+        transform.position = positions[playbackIndex];
+    }
+
+    void PlayRecording(KeyCode key)
+    {
+        if (Input.GetKeyDown(key))
+        {
+            recordingState = RecordingState.PLAYBACK;
+        }
+    }
+
+
 }
