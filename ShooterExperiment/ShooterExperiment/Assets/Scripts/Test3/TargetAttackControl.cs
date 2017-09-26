9727a1ade5ffe0764cc54ebf882cec273d41d1c5
DESKTOP-AV91D2I\paosa <paosalcedo@gmail.com>
2017-09-26 15:26:38 -0400

    Enemies can detect if someone within List.playersInCone is in List.playersInConeAndInSight; only works for one

--- ShooterExperiment/ShooterExperiment/Assets/Scripts/Test3/TargetAttackControl.cs
+++ ShooterExperiment/ShooterExperiment/Assets/Scripts/Test3/TargetAttackControl.cs
@@ -7,9 +7,10 @@ public class TargetAttackControl : MonoBehaviour {
 	private float cooldown;
 	private float rotationSpeed = 3f;
 
-	public GameObject[] players;
+	// public GameObject[] players;
 
 	public List<GameObject> playersInCone = new List<GameObject>();
+	public List<GameObject> playersInConeAndInSight = new List<GameObject>();	
 
 	public GameObject player1;
 	public GameObject player2;
@@ -40,7 +41,7 @@ public class TargetAttackControl : MonoBehaviour {
 	Vector3 player2Dir;
 	Vector3 player3Dir;
 	void Start () {
-		players = GameObject.FindGameObjectsWithTag("Player");
+		// players = GameObject.FindGameObjectsWithTag("Player");
 
 		player1Dir = player1.transform.position - transform.position;
 		player2Dir = player2.transform.position - transform.position;
@@ -70,7 +71,6 @@ public class TargetAttackControl : MonoBehaviour {
 				if(PlayerIsInConeOfVision()){
 					Debug.Log("player is in cone of vision!");
 					CheckIfInLineOfSight(closestVisiblePlayer);	
-					
 				}
  		
 				//return to normal pos and rot
@@ -84,7 +84,9 @@ public class TargetAttackControl : MonoBehaviour {
 				break;
 			
 			case AlertState.ALERTED:
-				// CheckIfInConeOfVision();
+				CheckIfInConeOfVision();
+				// CheckIfInLineOfSight(closestVisiblePlayer);	
+
 				if(playersInCone.Count > 0){
 					DetectNearestPlayer();
 					playerDir = closestPlayer.transform.position - transform.position;
@@ -94,6 +96,8 @@ public class TargetAttackControl : MonoBehaviour {
 						cooldown = EnemyDefs.enemyDict[EnemyDefs.EnemyType.TARGET].attackCooldown;
 					}
 					cooldown -= Time.deltaTime;
+
+
 				}
 
 				break;
@@ -123,20 +127,33 @@ public class TargetAttackControl : MonoBehaviour {
         // }
 
 		// closestPlayer = players[0];
-		for (int i = 0; i < playersInCone.Count; i++) {
-			distToPlayer[i] = Vector3.Distance(playersInCone[i].transform.position, transform.position);
-            for (int j = i+1; j < playersInCone.Count; j++) {
+		// for (int i = 0; i < playersInCone.Count; i++) {
+		// 	distToPlayer[i] = Vector3.Distance(playersInCone[i].transform.position, transform.position);
+        //     for (int j = i+1; j < playersInCone.Count; j++) {
+        //         if ( (distToPlayer[i] > distToPlayer[j]) && (i != j) ) {
+		// 			GameObject tempGameObject;
+		// 			tempGameObject = playersInCone[j];
+		// 			playersInCone[j] = playersInCone[i];
+		// 			playersInCone[i] = tempGameObject;
+        //         }
+        //     }
+        // }
+
+		// closestPlayer = playersInCone[0];
+
+		for (int i = 0; i < playersInConeAndInSight.Count; i++) {
+			distToPlayer[i] = Vector3.Distance(playersInConeAndInSight[i].transform.position, transform.position);
+            for (int j = i+1; j < playersInConeAndInSight.Count; j++) {
                 if ( (distToPlayer[i] > distToPlayer[j]) && (i != j) ) {
 					GameObject tempGameObject;
-					tempGameObject = playersInCone[j];
-					playersInCone[j] = playersInCone[i];
-					playersInCone[i] = tempGameObject;
+					tempGameObject = playersInConeAndInSight[j];
+					playersInConeAndInSight[j] = playersInConeAndInSight[i];
+					playersInConeAndInSight[i] = tempGameObject;
                 }
             }
         }
 
-		closestPlayer = playersInCone[0];
-		
+		closestPlayer = playersInConeAndInSight[0];
  	}
 	
 
@@ -151,19 +168,25 @@ public class TargetAttackControl : MonoBehaviour {
     //     }
 
 	//Changes alert state
+	private bool player1inCone = false;
+	private bool player2inCone = false;
+	private bool player3inCone = false;
 	public void CheckIfInConeOfVision(){
- 		if(Vector3.Dot(transform.forward, player1Dir) > 0f){
+ 		if(Vector3.Dot(transform.forward, player1Dir) > 0f && !player1inCone){
 			playersInCone.Add(player1);
+			player1inCone = true;
 		} 
 		
-		if(Vector3.Dot(transform.forward, player2Dir) > 0f)
+		if(Vector3.Dot(transform.forward, player2Dir) > 0f && !player2inCone)
 		{
 			playersInCone.Add(player2);
+			player2inCone = true;
 		}
 		
-		if(Vector3.Dot(transform.forward, player3Dir) > 0f)
+		if(Vector3.Dot(transform.forward, player3Dir) > 0f && !player3inCone)
 		{
 			playersInCone.Add(player3);
+			player3inCone = true;
 		}
 	}
 
@@ -173,23 +196,26 @@ public class TargetAttackControl : MonoBehaviour {
  			closestVisiblePlayer = player1;
 			playerIsInConeOfVision = true;
 			playersInCone.Add(player1);
+			player1inCone = true;
 
 		} if(Vector3.Dot(transform.forward, player2Dir) > 0f)
 		{
  			closestVisiblePlayer = player2;
 			playerIsInConeOfVision = true;
 			playersInCone.Add(player2);
+			player2inCone = true;
 		}
 		if(Vector3.Dot(transform.forward, player3Dir) > 0f)
 		{
  			closestVisiblePlayer = player3;
 			playerIsInConeOfVision = true;
 			playersInCone.Add(player3);
+			player3inCone = true;
 		}
 		return playerIsInConeOfVision;
 	}
 	
- 
+	
  	public void CheckIfInLineOfSight(GameObject closestVisiblePlayer_){
 		// RaycastHit hit;
  		// rayDirection = closestPlayer.transform.position - transform.position;
@@ -228,6 +254,7 @@ public class TargetAttackControl : MonoBehaviour {
  		Debug.DrawRay(transform.position, rayDirection * 5f, Color.red);
 		if (Physics.Raycast (transform.position, rayDirection, out hit, Mathf.Infinity, ownAmmo)) {
 			if(hit.transform.tag == "Player"){
+				playersInConeAndInSight.Add(hit.transform.gameObject);
 				alertState = AlertState.ALERTED;
 			}
 		}
